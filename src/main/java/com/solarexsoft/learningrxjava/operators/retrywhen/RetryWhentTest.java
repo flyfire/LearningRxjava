e160e40a6419fb9895c48bf3e967d778bbc24cd1
Solarex <rh.hou.work@gmail.com>
2018-03-13 18:03:49 +0800

    retry when

--- src/main/java/com/solarexsoft/learningrxjava/operators/retrywhen/RetryWhentTest.java
+++ src/main/java/com/solarexsoft/learningrxjava/operators/retrywhen/RetryWhentTest.java
@@ -1,8 +1,13 @@
 package com.solarexsoft.learningrxjava.operators.retrywhen;
 
 import io.reactivex.*;
+import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 
 import java.util.concurrent.TimeUnit;
 
@@ -10,14 +15,13 @@ import java.util.concurrent.TimeUnit;
  * Created by houruhou on 13/03/2018.
  */
 public class RetryWhentTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Observable.create(new ObservableOnSubscribe<String>() {
             @Override
             public void subscribe(ObservableEmitter<String> e) throws Exception {
-                System.out.println("emitting");
-                Thread.sleep(1000);
-                int i = 2/0;
+                e.onNext("hello");
+                e.onNext("world");
                 e.onError(new Throwable("always fail"));
             }
         }).retryWhen(new Function<Observable<Throwable>, ObservableSource<?>>() {
@@ -27,7 +31,7 @@ public class RetryWhentTest {
                     @Override
                     public ObservableSource<?> apply(Throwable throwable) throws Exception {
                         System.out.println(throwable);
-                        return Observable.just(1).timer(1, TimeUnit.SECONDS);
+                        return Observable.just(1).delay(1, TimeUnit.SECONDS);
                     }
                 });
             }
@@ -52,7 +56,7 @@ public class RetryWhentTest {
                 System.out.println("oncomplete");
             }
         });
-        /*
+
         Flowable.create(new FlowableOnSubscribe<String>() {
             @Override
             public void subscribe(FlowableEmitter<String> e) throws Exception {
@@ -91,6 +95,61 @@ public class RetryWhentTest {
             public void onComplete() {
                 System.out.println("oncomplete");
             }
-        });*/
+        });
+
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                e.onNext(1);
+                e.onNext(2);
+                e.onError(new Exception("发生错误了"));
+                e.onNext(3);
+            }
+        })
+                .retryWhen(new Function<Observable<Throwable>, ObservableSource<?>>() {
+                    @Override
+                    public ObservableSource<?> apply(@NonNull Observable<Throwable> throwableObservable) throws Exception {
+
+                        // 返回新的被观察者 Observable
+                        // 此处有两种情况：
+                        // 1. 原始的Observable不重新发送事件：新的被观察者 Observable发送的事件 = Error事件
+                        // 2. 原始的Observable重新发送事件：新的被观察者 Observable发送的事件 = 数据事件
+                        return throwableObservable.flatMap(new Function<Throwable, ObservableSource<?>>() {
+                            @Override
+                            public ObservableSource<?> apply(@NonNull Throwable throwable) throws Exception {
+
+                                // 1. 若返回的Observable发送的事件 = Error事件，则原始的Observable不重新发送事件
+                                // 该异常错误信息可在观察者中的onError（）中获得
+                                // return Observable.error(new Throwable("retryWhen终止啦"));
+
+                                // 2. 若返回的Observable发送的事件 = 数据事件，则原始的Observable重新发送事件（若持续遇到错误，则持续重试）
+                                return Observable.just(1).delay(1, TimeUnit.SECONDS);
+                            }
+                        });
+
+                    }
+                })
+                .subscribeOn(Schedulers.newThread()).subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        System.out.println("onsubscribe--");
+                    }
+                    @Override
+                    public void onNext(Integer value) {
+                        System.out.println("接收到了事件"+ value  );
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        System.out.println("对Error事件作出响应" + e.toString());
+                        // 获取异常错误信息
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("对Complete事件作出响应");
+                    }
+                });
+        Thread.sleep(10000);
     }
 }
